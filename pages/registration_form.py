790ba00293597273272ebfa1753f4e5331383f4e
Karolina Zakrzewska <92153501+KarolinaZakrzewska@users.noreply.github.com>
2022-07-07 08:32:52 +0200

    Add files via upload

---
+++ pages/registration_form.py
@@ -0,0 +1,140 @@
+from selenium import webdriver
+import pytest
+from time import sleep
+from selenium.webdriver.common.by import By
+import unittest
+from selenium.webdriver.support.select import Select
+
+#from faker import Faker
+
+#fake = Faker()
+
+
+#All selectors used in these methods
+email_add_id = "email_create"
+first_name_id = "customer_firstname"
+create_account_id = "SubmitCreate"
+last_name_id = "customer_lastname"
+password_id = "passwd"
+address_id = "address1"
+first_name_address_id = "firstname"
+last_name_address_id = "lastname"
+city_id = "city"
+state_id = "id_state"
+postal_code_id = "postcode"
+country_id = "id_country"
+phone_id = "phone_mobile"
+alias_id = "alias"
+register_id = "submitAccount"
+error_message_xpath = '//div[@class="alert alert-danger"]/p'
+
+
+
+
+#All variables
+#email_input = fake.email()
+email_input = "testfd@gmail.com"
+first_name_input = "ania"
+last_name_input = "Ryba"
+password_input = "Abcde123456!"
+birthdate_select = "1996-12-27"
+days = "days"
+months = "months"
+years = "years"
+address_input = "Grzybowska 56"
+city_input = "Warsaw"
+state_select = "Texas"
+postal_code_input = "02123"
+country_select = "United States"
+phone_input = "123234234"
+alias_input = "warszawski"
+
+class RegistrationForm:
+
+  def set_email(self):
+    driver = self.driver
+    driver.find_element(By.ID, email_add_id).send_keys(email_input)
+
+  def click_button_create_account(self):
+    driver = self.driver
+    driver.find_element(By.ID, create_account_id).click()
+
+  def set_first_name(self):
+    driver = self.driver
+    driver.find_element(By.ID, first_name_id).send_keys(first_name_input)
+
+  def set_last_name(self):
+    driver = self.driver
+    driver.find_element(By.ID, last_name_id).send_keys(last_name_input)
+
+  def set_password(self):
+    driver = self.driver
+    driver.find_element(By.ID, password_id).send_keys(password_input)
+
+  def set_date_birth(self):
+    driver = self.driver
+
+    birthday_list = birthdate_select.split("-")
+    birthday = str(int(birthday_list[2]))
+    birthmonth = birthday_list[1]
+    birthyear = birthday_list[0]
+
+    days_s = Select(driver.find_element(By.ID, days))
+    days_s.select_by_value(birthday)
+
+    months_s = Select(driver.find_element(By.ID, months))
+    months_s.select_by_value(birthmonth)
+
+    years_s = Select(driver.find_element(By.ID, years))
+    years_s.select_by_value(birthyear)
+
+  def set_first_name_address_or_equal_first_name_input(self):
+    driver = self.driver
+    assert driver.find_element(By.ID, first_name_address_id).get_attribute("value") == first_name_input, "Assertion passed, Expected first name with adress equal first name input"
+
+  def set_last_name_address_or_equal_last_name_input(self):
+    driver = self.driver
+    assert driver.find_element(By.ID, last_name_address_id).get_attribute("value") == last_name_input, "Assertion passed, Expected last name with adress equal last name input"
+
+  def set_address(self):
+    driver = self.driver
+    driver.find_element(By.ID, address_id).send_keys(address_input)
+
+  def set_city(self):
+    driver = self.driver
+    driver.find_element(By.ID, city_id).send_keys(city_input)
+
+  def set_state(self):
+    driver = self.driver
+    Select(driver.find_element(By.ID, state_id))
+    driver.find_element(By.ID, state).send_keys(state_select)
+
+  def set_postal_code(self):
+    driver = self.driver
+    driver.find_element(By.ID, postal_code).send_keys(postal_code_input)
+
+  def set_country(self):
+    driver = self.driver
+    Select(driver.find_element(By.ID, country_id))
+    driver.find_element(By.ID, country).send_keys(country_select)
+
+  def set_phone(self):
+    driver = self.driver
+    driver.find_element(By.ID, phone_id).send_keys(phone_input)
+
+  def set_alias(self):
+    driver = self.driver
+    driver.find_element(By.ID, alias_id).clear()
+    driver.find_element(By.ID, alias_id).send_keys(alias_input)
+
+  def set_register(self):
+    driver = self.driver
+    driver.find_element(By.ID, register_id).click()
+
+  def error_message(self):
+    driver = self.driver
+    assert (driver.find_element(By.XPATH, error_message_xpath)).text == "There is 1 error", "Assertion passed, Expected last name with adress equal last name input"
+
+
+    # error_messages = driver.find_elements(By.XPATH, '//div[@class="alert alert-danger"]//li')
+    # self.assertEqual("firstname is required.", error_messages[0].text)
---
+++ tests/test_registration_form.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from pages.registration_form import RegistrationForm
+#from time import sleep
+import unittest
+from selenium.webdriver.support.select import Select
+
+class TestRegistrationForm:
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(2)
+    driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account")
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+
+    def test_email_input_validation(self):
+        RegistrationForm.set_email(self)
+
+    def test_click_button_create_account_validation(self):
+        RegistrationForm.click_button_create_account(self)
+
+    def test_first_name_input_validation(self):
+        RegistrationForm.set_first_name(self)
+
+    def test_last_name_input_validation(self):
+        RegistrationForm.set_last_name(self)
+
+    def test_password_input_validation(self):
+        RegistrationForm.set_password(self)
+
+    def test_date_birth_select_validation(self):
+        RegistrationForm.set_date_birth(self)
+
+    def test_first_name_address_or_equal_first_name_input_validation(self):
+        RegistrationForm.set_first_name_address_or_equal_first_name_input(self)
+
+    def test_last_name_address_or_equal_last_name_input_validation(self):
+        RegistrationForm.set_last_name_address_or_equal_last_name_input(self)
+
+    def test_address_input_validation(self):
+        RegistrationForm.set_address(self)
+
+    def test_city_input_validation(self):
+        RegistrationForm.set_city(self)
+
+    def test_state_select_validation(self):
+        RegistrationForm.set_state(self)
+
+    def test_postal_code_input_validation(self):
+        RegistrationForm.set_postal_code(self)
+
+    def test_country_input_validation(self):
+        RegistrationForm.set_country(self)
+
+    def test_phone_input_validation(self):
+        RegistrationForm.set_phone(self)
+
+    def test_alias_input_validation(self):
+        RegistrationForm.set_alias(self)
+
+    def test_click_register_validation(self):
+        RegistrationForm.set_register(self)
+
+    def test_error_message_validation(self):
+        RegistrationForm.error_message(self)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__== "__main__":
+    unittest.main(verbosity = 2)
